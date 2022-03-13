732e327d15323ea06bc4740a520cef5679139710
JuwonIshola <juwonishola19@gmail.com>
2022-03-13 19:00:49 +0000

    program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
  * Description: Print all possible combinations of two two-digit numbers.
  * Numbers should range from 0 to 99.
  * The two numbers should be separated by a space.
@@ -18,34 +15,21 @@
  */
 int main(void)
 {
-int c;
-int d;
-int e;
-int f = 0;
 	int i, j;
 	int a, b, c, d;
 
-while (f < 10)
-{
-	e = 0;
-	while (e < 10)
 	for (i = 0; i < 100; i++)
 	{
-		d = 0;
-		while (d < 10)
 		a = i / 10; /* doubles fnum */
 		b = i % 10; /* singles fnum */
 
 		for (j = 0; j < 100; j++)
 		{
-			c = 0;
-			while (c < 10)
 			c = j / 10; /* doubles snum */
 			d = j % 10; /* singles snum */
 
 			if (a < c || (a == c && b < d))
 			{
-				if (!(f == c && e == d))
 				putchar(a + '0');
 				putchar(b + '0');
 				putchar(32);
@@ -54,29 +38,12 @@ while (f < 10)
 
 				if (!(a == 9 && b == 8))
 				{
-					putchar('0' + f);
-					putchar('0' + e);
-					putchar(' ');
-					putchar('0' + d);
-					putchar('0' + c);
-					if (!(f + e == 18 && c + d == 17 && d == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
 					putchar(44);
 					putchar(32);
 				}
-				c++;
 			}
-			d++;
 		}
-		e++;
 	}
-	f++;
-}
-putchar('\n');
-return (0);
 	putchar(10);
 
 	return (0);
