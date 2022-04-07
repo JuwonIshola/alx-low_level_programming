ea7bb915de353282c3c6acf48623606bed7b5a25
Juwon Ishola <juwonishola19@gmail.com>
2022-04-07 15:20:33 +0100

    function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,48 +1,43 @@
-#include <stdlib.h>
-
+#include "main.h"
 /**
- * argstostr - Concatenate all arguments of a program
- * @ac: Arg count
- * @av: Array of arguments
- *
- * Return: Pointer to new string, NULL if it fails
+ * argstostr - prints args
+ * @ac: takes in width of grid
+ * @av: height of grid
+ * Return: the args one line at a time
  */
+
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int i, j, k, len;
+	int count = 0, a = 0, b = 0, c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	i = len = 0;
-	while (i < ac)
+	while (a < ac)
 	{
-		j = 0;
-		while (av[i][j] != '\0')
+		b = 0;
+		while (av[a][b] != '\0')
 		{
-			len++;
-			j++;
+			count++;
+			b++;
 		}
-		len++;
-		i++;
+		a++;
 	}
-	len++;
-	str = malloc(len * sizeof(char));
+	count = count + ac + 1;
+	str = malloc(sizeof(char) * count);
 	if (str == NULL)
-		return (NULL);
-	i = k = 0;
-	while (i < ac)
 	{
-		j = 0;
-		while (av[i][j] != 0)
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
 		{
-			str[k] = av[i][j];
-			k++;
-			j++;
+			str[c] = av[a][b];
+			c++;
 		}
-		str[k] = '\n';
-		k++
-			i++
+		str[c] = '\n';
+		c++;
 	}
 	return (str);
 }
